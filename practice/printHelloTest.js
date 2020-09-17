2546bd2da3dc436096bb52d593e956ec741950ce
structsize <52183019+structsize@users.noreply.github.com>
2020-09-17 14:20:07 +0900

    [UPDATE] 9/17 class

--- practice/printHelloTest.js
+++ practice/printHelloTest.js
@@ -1,3 +1,11 @@
 var md = require("./printHello")
+const readline = require('readline')
 
-md.helloWorld(4)
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+//md.helloWorld(4)
+rl.question("몇번 반복 할까요?", (answer) =>
+md.helloWorld(answer),
+rl.close)
